4350d8d2ba78f24f1d162f9cff90b5a245374fc4
Jessica <jessicateixeira32@gmail.com>
2022-01-02 20:19:13 +0000

    [upd] Scene with first colour scheme
    Added colour to the static cubes
    Added the collectable cubes
    Added the light beams where to deliver the collectables
    Added changing colour cubes according to time variable

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    public Material[] colors;
+    public float[] duration;
+    private int i;
+    private Renderer m;
+
+
+    private void Start()
+    {
+        m = GetComponent<Renderer>();
+        i = 0;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (i >= colors.Length) 
+        {
+            i = 0;
+        }
+        
+        if (duration[i] > 0)
+        {
+            m.material = colors[i];
+            duration[i] -= Time.deltaTime;
+            
+        }
+        else
+        {
+            duration[i] = 1.5f;
+            i += 1;
+        }
+     
+
+
+        
+
+
+
+    }
+}
