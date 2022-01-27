1a0b7097ce77204136aeb037ebf277ab68294d88
skirobr <35534264+skirobr@users.noreply.github.com>
2022-01-27 23:20:18 +0000

    Pega os cubos e deixa nos pilares
    Sistema de pegar itens no chão e deixar no foi refeito, e por proximidade

---
+++ Assets/Code/Gabriel/Player/GroundDetectionv2.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GroundDetectionv2 : MonoBehaviour
+{
+    string tagName = "";
+    private LayerMask ground;
+    private Vector3 startPosition;
+    private string currentSceneName;
+    private void Start()
+    {
+        ground = LayerMask.NameToLayer("Ground");
+        startPosition = transform.parent.transform.position;
+        cubeColor = "White";
+        currentSceneName = SceneManager.GetActiveScene().name;
+    }
+
+    [SerializeField] public GameObject keyRed, keyBlue, keyWhite, keyYellow;
+    private void OnTriggerEnter(Collider other)
+    {
+        tagName = other.gameObject.tag;
+        if (ground == other.gameObject.layer)
+        {
+            if (cubeColor != "" && tagName != "Gray")
+            {
+                Debug.Log("Ground Tag " + tagName + " cubeColor -> " + cubeColor);
+                if (tagName != cubeColor)//reseta o cenario se pisar errado
+                {
+                    SceneManager.LoadScene(currentSceneName);
+                }
+            }
+        }
+    }
+
+    [SerializeField] public string cubeColor = "White";
+    public void CubeColor(string _cubeColor)
+    {
+        cubeColor = _cubeColor;
+    }
+}
